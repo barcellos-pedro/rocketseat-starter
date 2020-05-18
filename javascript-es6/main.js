1786011bdce580dfbaeee0f8af49b113f5f85e7d
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-05-18 16:33:06 -0300

    Iniciar ES6+. Aula - Classes.

---
+++ javascript-es6/main.js
@@ -0,0 +1,20 @@
+class List{
+    
+}
+
+class TodoList{
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push("Novo todo");
+        console.log(this.todos);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.querySelector("#novotodo").onclick = function(){
+    MinhaLista.addTodo();
+}
